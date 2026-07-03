57a85b4e7750b9a8f5e4c28e29ef164bf9ddb3af
Ilya Naumenko <naumenko951294@yandex.ru>
2026-07-03 14:33:17 +0300

    Выполнение финального ТЗ спринта 10

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,69 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int nextId = 1;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Film create(@RequestBody Film film) {
+        validateFilm(film);
+        film.setId(nextId++);
+        films.put(film.getId(), film);
+        log.info("Фильм добавлен: {}", film.getName());
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film film) {
+        // 1. Сначала проверяем, есть ли такой фильм в базе
+        if (!films.containsKey(film.getId())) {
+            // Если нет — кидаем RuntimeException.
+            // Spring Boot по умолчанию вернет за него 500 ошибку.
+            throw new RuntimeException("Фильм с id " + film.getId() + " не найден");
+        }
+
+        // 2. Только если фильм найден, проводим валидацию данных
+        validateFilm(film);
+
+        // 3. Обновляем данные
+        films.put(film.getId(), film);
+        log.info("Фильм обновлён: {}", film.getName());
+        return film;
+    }
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    private void validateFilm(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ValidationException("Название фильма не может быть пустым");
+        }
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            throw new ValidationException("Максимальная длина описания — 200 символов");
+        }
+        if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+        if (film.getDuration() <= 0) {
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+        }
+    }
 }
